c9353aaee3d36ed0d995f92b6f079d4ad40f19a5
pandu5164 <zpkplay@gmail.com>
2024-01-22 02:47:07 +0530

    new changes

---
+++ src/Components/MovieListPage/MovieListPage.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import {
+  LazyLoadImage,
+  trackWindowScroll,
+} from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "./movielistpage.scss";
+import defaultImage from "./diagnal_logo.jpeg";
+const MovieListPage = ({
+  movieList = [],
+  makeImageApiCall = () => {},
+  totalContentItem,
+}) => {
+  const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 });
+  const handleScroll = (event) => {
+    const target = event.target.scrollingElement;
+    setScrollPosition({ x: target.scrollWidth, y: target.scrollHeight });
+    if (target.scrollHeight - target.scrollTop === target.clientHeight) {
+      makeImageApiCall();
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div className="movie-list">
+      {!movieList.length && <p>No data to show</p>}
+      {movieList.length &&
+        movieList?.map((image, id) => (
+          <div key={id} className="movie-item">
+            <LazyLoadImage
+              wrapperClassName="movie-banner"
+              key={id}
+              alt={image.name}
+              effect="blur"
+              threshold={100}
+              width={100} //2 times width
+              height={150} // 3 times height
+              placeholderSrc={defaultImage}
+              scrollPosition={scrollPosition}
+              src={`https://test.create.diagnal.com/images/${image["poster-image"]}`}
+              visibleByDefault={image.src === defaultImage}
+            />
+            <p className="movie-name" title={image.name}>
+              {image.name}
+            </p>
+          </div>
+        ))}
+      <br />
+      {totalContentItem === movieList.length && movieList.length && (
+        <p className="end-of-list">--- End of list ---</p>
+      )}
+    </div>
+  );
+};
+export default trackWindowScroll(MovieListPage);
