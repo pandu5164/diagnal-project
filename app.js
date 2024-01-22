a2cd7a8f4ab857c58b5cc2c8e41e5cd03f03dd30
pandu5164 <zpkplay@gmail.com>
2024-01-22 15:30:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000;
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
