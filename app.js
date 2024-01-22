1d4dad00875fe1478558b7e73f4cc8c3e2bc9b59
pandu5164 <zpkplay@gmail.com>
2024-01-22 15:30:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// app.use(express.static(path.join(__dirname, 'build')));
-
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 // app.get('/', function (req, res) {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
